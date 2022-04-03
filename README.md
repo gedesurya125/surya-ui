5edf0258f74f114700cce0bc43802e947dc62a7d
gedesurya125 <gedesurya125@gmail.com>
2022-04-03 21:24:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# [theme-ui](https://theme-ui.com/) and [framer-motion](https://www.framer.com/motion/) Combined
---
+++ src/components/MotionComponents.tsx
@@ -0,0 +1,72 @@
+import {
+  Box as ThemeUiBox,
+  Flex as ThemeUiFlex,
+  Grid as ThemeUiGrid,
+  Button as ThemeUiButton,
+  Text as ThemeUiText,
+  Heading as ThemeUiHeading,
+  Link as ThemeUiLink,
+  Paragraph as ThemeUiParagraph,
+  Image as ThemeUiImage,
+  Card as ThemeUiCard,
+  Label as ThemeUiLabel,
+  Input as ThemeUiInput,
+  Select as ThemeUiSelect,
+  Textarea as ThemeUiTextarea,
+  Radio as ThemeUiRadio,
+  Checkbox as ThemeUiCheckbox,
+  Slider as ThemeUiSlider,
+  Switch as ThemeUiSwitch,
+  Field as ThemeUiField,
+  Progress as ThemeUiProgress,
+  Donut as ThemeUiDonut,
+  Avatar as ThemeUiAvatar,
+  Badge as ThemeUiBadge,
+  Close as ThemeUiClose,
+  Alert as ThemeUiAlert,
+  Divider as ThemeUiDivider,
+  Embed as ThemeUiEmbed,
+  AspectRatio as ThemeUiAspectRatio,
+  AspectImage as ThemeUiAspectImage,
+  Container as ThemeUiContainer,
+  NavLink as ThemeUiNavLink,
+  Message as ThemeUiMessage,
+  IconButton as ThemeUiIconButton,
+  MenuButton as ThemeUiMenuButton,
+} from 'theme-ui';
+import { motion } from 'framer-motion';
+
+export const Box = motion(ThemeUiBox);
+export const Flex = motion(ThemeUiFlex);
+export const Grid = motion(ThemeUiGrid);
+export const Button = motion(ThemeUiButton);
+export const Text = motion(ThemeUiText);
+export const Heading = motion(ThemeUiHeading);
+export const Link = motion(ThemeUiLink);
+export const Paragraph = motion(ThemeUiParagraph);
+export const Image = motion(ThemeUiImage);
+export const Card = motion(ThemeUiCard);
+export const Label = motion(ThemeUiLabel);
+export const Input = motion(ThemeUiInput);
+export const Select = motion(ThemeUiSelect);
+export const Textarea = motion(ThemeUiTextarea);
+export const Radio = motion(ThemeUiRadio);
+export const Checkbox = motion(ThemeUiCheckbox);
+export const Slider = motion(ThemeUiSlider);
+export const Switch = motion(ThemeUiSwitch);
+export const Field = motion(ThemeUiField);
+export const Progress = motion(ThemeUiProgress);
+export const Donut = motion(ThemeUiDonut);
+export const Avatar = motion(ThemeUiAvatar);
+export const Badge = motion(ThemeUiBadge);
+export const Close = motion(ThemeUiClose);
+export const Alert = motion(ThemeUiAlert);
+export const Divider = motion(ThemeUiDivider);
+export const Embed = motion(ThemeUiEmbed);
+export const AspectRatio = motion(ThemeUiAspectRatio);
+export const AspectImage = motion(ThemeUiAspectImage);
+export const Container = motion(ThemeUiContainer);
+export const NavLink = motion(ThemeUiNavLink);
+export const Message = motion(ThemeUiMessage);
+export const IconButton = motion(ThemeUiIconButton);
+export const MenuButton = motion(ThemeUiMenuButton);
